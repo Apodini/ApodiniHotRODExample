8c2fd79b0d8697208851e37c92b98675896a40db
Moritz Sternemann <m.sternemann@tum.de>
2021-11-10 18:52:56 +0100

    Add example tracing configuration

--- Sources/WebService/ExampleWebService.swift
+++ Sources/WebService/ExampleWebService.swift
@@ -7,9 +7,13 @@
 //
 
 import Apodini
+import ApodiniObserve
 import ApodiniOpenAPI
 import ApodiniREST
 import ArgumentParser
+import Logging
+import OpenTelemetry
+import OtlpGRPCSpanExporting
 
 
 @main
@@ -23,6 +27,28 @@ struct ExampleWebService: WebService {
         REST {
             OpenAPI()
         }
+
+        TracingConfiguration(
+            // Export to OpenTelemetry collector via gRPC and the OpenTelemetry protocol (OTLP)
+            .defaultOpenTelemetry,
+
+            // Expose configuration options from opentelemetry-swift
+            .openTelemetryWithConfig(
+                resourceDetection: .automatic(additionalDetectors: []),
+                idGenerator: OTel.RandomIDGenerator(),
+                sampler: OTel.ConstantSampler(isOn: true),
+                processor: { group in
+                    OTel.SimpleSpanProcessor(
+                        exportingTo: OtlpGRPCSpanExporter(config: .init(eventLoopGroup: group))
+                    )
+                },
+                propagator: OTel.W3CPropagator(),
+                logger: Logger(label: "ApodiniOTel")
+            )
+
+            // Any other instrument conforming to swift-distributed-tracing's `Instrument`
+            // InstrumentConfiguration(MyAwesomeInstrument())
+        )
     }
     
     var content: some Component {
