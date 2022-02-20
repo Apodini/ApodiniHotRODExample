4985c8e92fb1e142947611ae6024cf514d24fec4
Moritz Sternemann <m.sternemann@tum.de>
2022-02-20 15:21:05 +0100

    Add missing licenses

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+#
+# This source file is part of the Apodini HotROD example open source project
+#
+# SPDX-FileCopyrightText: 2022 Paul Schmiedmayer and the project authors (see CONTRIBUTORS.md) <paul.schmiedmayer@tum.de>
+#
+# SPDX-License-Identifier: MIT
+#
+
 URL=http://localhost:8080/config
 ITERATIONS=1000
 
